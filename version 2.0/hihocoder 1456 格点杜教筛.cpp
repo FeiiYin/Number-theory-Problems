e9c69130823adf8e93da2034d71c7757bd4110df
Samaritan <1594320575@qq.com>
2018-09-27 17:31:38 +0800

    Update hihocoder 1456 格点杜教筛.cpp

--- version 2.0/hihocoder 1456 格点杜教筛.cpp
+++ version 2.0/hihocoder 1456 格点杜教筛.cpp
@@ -1,3 +1,10 @@
 https://shichengxiao01.github.io/2018/03/14/Hihocoder1456%20Rikka%20with%20Lattice/#more
 
 https://www.cnblogs.com/zjp-shadow/p/8492876.html
+
+/// 题意: 给一个 n * m 的
+///皮克定理:指一个计算点阵中顶点在格点上的多边形面积公式，该公式为2S=2a+b-2，
+/// 其中a表示多边形内部的点数，b表示多边形边界上的点数，S表示多边形的面积。
+
+/// 裴蜀定理: 一定存在整数x,y，使ax+by=gcd(a, b)成立，可以推广到n个数
+///  　　　　　反过来，要ax+by=c有解，需要gcd(a, b) = c
